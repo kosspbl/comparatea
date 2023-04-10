b7a28134857e66003adc0819a5bbf958aba93fe8
peetank@fedora <peetank@github.com>
2023-04-11 04:21:49 +0530

    added amazon.py and change css - now cheapest option from amazon and flipkart is shown - made things a little prettier than before in css

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, render_template #importing flask libraries
+import flipkart
+import amazon
+
 
 print("This is app.py")
 
@@ -7,15 +10,41 @@ app = Flask(__name__)
 @app.route('/', methods = ["GET", "POST"])
 def index_page():
     search_key = ""
-    filename = ""
-    link = ""
-    pics = ""
+
+    #for flipkart
+    fProductName = ""
+    fLink = ""
+    fPics = ""
+    fPrice = ""
+
+    #for amazon
+    aProductName = ""
+    aLink = ""
+    aPics = ""
+    aPrice = ""
+    
+    #for declaring winnner
+    fwin = ""
+    awin = ""
+
+
     if request.method == "POST" :
-        import flipkart
         search_key = str(request.form.get("name"))
         search_key.replace(" ", "+")
-        filename, link, pics = flipkart.first_name(search_key)
-    return render_template('index.html', result1 = "First Product :  " + str(filename), result2 = link, result3 = pics)
+        fProductName, fPrice, fLink, fPics = flipkart.first_name(search_key)
+        aProductName, aPrice, aLink, aPics = amazon.first_name(search_key)
+
+        if fPrice <= aPrice:
+            fwin = "Winner"
+        else:
+            awin = "Winner"
+    return render_template('index.html',
+                           result01 = fwin, result02 = awin,
+                           result1 = "Cheapest on Flipkart : " + str(fProductName),
+                           result2 = fLink, result3 = fPics,
+                           result4 = "Cheapest on Amazon : " + str(aProductName),
+                           result5 = aLink, result6 = aPics,
+                           resultp1 = fPrice, resultp2 = aPrice)
 
 if __name__ == '__main__':
     app.run()
